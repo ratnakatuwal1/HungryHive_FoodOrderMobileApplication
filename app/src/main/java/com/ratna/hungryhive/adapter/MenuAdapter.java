8662194b5a0a765012bf89053f7fe62cbcd79bbc
Ratna Katuwal <115388990+Ratna-katuwal1@users.noreply.github.com>
2024-07-24 23:06:18 +0545

    product description start

--- app/src/main/java/com/ratna/hungryhive/adapter/MenuAdapter.java
+++ app/src/main/java/com/ratna/hungryhive/adapter/MenuAdapter.java
@@ -1,11 +1,14 @@
 package com.ratna.hungryhive.adapter;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.ratna.hungryhive.FoodDescriptionActivity;
 import com.ratna.hungryhive.databinding.MenuItemBinding;
 
 import java.util.List;
@@ -47,6 +50,14 @@ public class MenuAdapter extends RecyclerView.Adapter<MenuAdapter.MenuViewHolder
         public MenuViewHolder(@NonNull MenuItemBinding binding) {
             super(binding.getRoot());
             this.binding = binding;
+
+            binding.getRoot().setOnClickListener(view -> {
+                Context context = view.getContext();
+                Intent intent = new Intent(context, FoodDescriptionActivity.class);
+                intent.putExtra("menuItemName", binding.menuFoodName.getText().toString());
+                intent.putExtra("menuImage", (Intent) view.getTag());
+                context.startActivity(intent);
+            });
         }
 
         public void bind(String item, String price, int image) {
