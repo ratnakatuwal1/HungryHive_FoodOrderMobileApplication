0fc97848c6ddcd48981be5b3e5f07639fc68f93d
Ratna Katuwal <115388990+Ratna-katuwal1@users.noreply.github.com>
2024-08-03 23:10:17 +0545

    small backend part and customer ui completed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.ratna.hungryhive"
-        minSdk = 24
+        minSdk = 28
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
