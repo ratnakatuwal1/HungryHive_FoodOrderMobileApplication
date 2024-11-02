682a29c7ea1ed25caaa72becfe0d50749e43974c
Ratna Katuwal <parbesh456@gmail.com>
2024-11-02 14:06:58 +0545

    initial commit

--- app/src/main/java/com/ratna/hungryhive/ChangePassword.java
+++ app/src/main/java/com/ratna/hungryhive/ChangePassword.java
@@ -1,6 +1,11 @@
 package com.ratna.hungryhive;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +13,70 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class ChangePassword extends AppCompatActivity {
+    EditText editTextPassword, editTextNewPassword, editTextConfirmPassword;
+    Button buttonChangePassword;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_change_password);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        editTextNewPassword = findViewById(R.id.editTextNewPassword);
+        editTextConfirmPassword = findViewById(R.id.editTextTextPassword3);
+        buttonChangePassword = findViewById(R.id.buttonChangePassword);
 
+        mAuth = FirebaseAuth.getInstance();
+
+        buttonChangePassword.setOnClickListener(v -> {
+            changePassword();
+        });
+
+    }
+
+    private void changePassword() {
+        String currentPassword = editTextPassword.getText().toString().trim();
+        String newPassword = editTextNewPassword.getText().toString().trim();
+        String confirmPassword = editTextConfirmPassword.getText().toString().trim();
+
+        if (TextUtils.isEmpty(currentPassword) || TextUtils.isEmpty(newPassword) || TextUtils.isEmpty(confirmPassword)){
+            Toast.makeText(this, "All field are required", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (!newPassword.equals(confirmPassword)){
+            Toast.makeText(this, "New password and confirm password is not match", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user != null && user.getEmail() != null){
+            AuthCredential credential = EmailAuthProvider.getCredential(user.getEmail(), currentPassword);
+
+            user.reauthenticate(credential).addOnCompleteListener(task -> {
+                if (task.isSuccessful()){
+                    user.updatePassword(newPassword).addOnCompleteListener(updateTask -> {
+                        if (updateTask.isSuccessful()){
+                            Toast.makeText(this, "Password updated successfully", Toast.LENGTH_SHORT).show();
+                            finish();
+                } else {
+                            Toast.makeText(this, "Failed to update password", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                } else {
+                            Toast.makeText(this, "Authentication failed. Incorrect current password", Toast.LENGTH_SHORT).show();
+                        }
+            });
+
+        } else {
+            Toast.makeText(this, "User not found", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/ratna/hungryhive/EditProfile.java
+++ app/src/main/java/com/ratna/hungryhive/EditProfile.java
@@ -1,19 +1,32 @@
 package com.ratna.hungryhive;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.ratna.hungryhive.model.UserModel;
+import com.squareup.picasso.Picasso;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -24,6 +37,16 @@ public class EditProfile extends AppCompatActivity {
     private Uri imageUri;
     private FirebaseAuth mAuth;
     private DatabaseReference databaseReference;
+
+    private final ActivityResultLauncher<Intent> selectImageLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+        if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+            imageUri = result.getData().getData();
+            // Handle the selected image URI
+            if (imageUri  != null){
+                Picasso.get().load(imageUri).into(profileImageChange);
+            }
+        }
+    });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,5 +64,84 @@ public class EditProfile extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
         databaseReference = FirebaseDatabase.getInstance().getReference("Users");
+        
+        loadUserData();
+        buttonChangeProfileImage.setOnClickListener(v -> {
+            openGallery();
+        });
+
+        buttonSaveChanges.setOnClickListener(v -> {
+            saveUserData();
+        });
+        
+    }
+
+    private void loadUserData() {
+        String userId = mAuth.getCurrentUser().getUid();
+        databaseReference.child(userId).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()){
+                    UserModel userProfile = snapshot.getValue(UserModel.class);
+                    if (userProfile != null){
+                        editTextName.setText(userProfile.getName());
+                        editTextEmailAddress.setText(userProfile.getEmail());
+                        editTextPhone.setText(userProfile.getPhone());
+                        editTextAddress.setText(userProfile.getAddress());
+
+                        if (userProfile.getProfileImageUrl() != null){
+                            Picasso.get().load(userProfile.getProfileImageUrl()).into(profileImageChange);
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(EditProfile.this, "Failed to load data", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void openGallery(){
+        Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        selectImageLauncher.launch(intent);
+    }
+
+    private void saveUserData(){
+        String userId = mAuth.getCurrentUser().getUid();
+        String name = editTextName.getText().toString().trim();
+        String address = editTextAddress.getText().toString().trim();
+        String phone = editTextPhone.getText().toString().trim();
+        String email = editTextEmailAddress.getText().toString().trim();
+
+        if (name.isEmpty() || address.isEmpty() || phone.isEmpty() || email.isEmpty()){
+            Toast.makeText(this, "Please fill all the fields", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        databaseReference.child(userId).child("name").setValue(name);
+        databaseReference.child(userId).child("address").setValue(address);
+        databaseReference.child(userId).child("phone").setValue(phone);
+        databaseReference.child(userId).child("email").setValue(email);
+
+        if (imageUri != null){
+            uploadProfileImage(userId);
+        } else {
+            Toast.makeText(this, "profile updated successfully", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void uploadProfileImage(String userId) {
+        StorageReference fileRef = FirebaseStorage.getInstance().getReference("profile_photos").child(userId + ".jpg");
+        fileRef.putFile(imageUri).addOnSuccessListener(taskSnapshot -> {
+            fileRef.getDownloadUrl().addOnSuccessListener(uri -> {
+                databaseReference.child(userId).child("profileImageUrl").setValue(uri.toString());
+                Toast.makeText(EditProfile.this, "Profile image updated successfully", Toast.LENGTH_SHORT).show();
+                finish();
+            });
+        }).addOnFailureListener(e -> {
+            Toast.makeText(EditProfile.this, "Failed to upload image", Toast.LENGTH_SHORT).show();
+        });
     }
 }
